06fa755c4ad1538e11f3c741e7d4728a5db569e7
java123413 <1677041520@qq.com>
2021-01-07 17:49:40 +0800

    lh

--- src/main/java/cn/net/yto/YtoApplication.java
+++ src/main/java/cn/net/yto/YtoApplication.java
@@ -2,6 +2,7 @@ package cn.net.yto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author ZHT
